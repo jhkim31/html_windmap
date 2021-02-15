cd1ddca71e62ca2c602e263de1c3b0b0f618bf5c
jhkim14 <jhkim79797728@gmail.com>
2021-02-15 13:38:10 +0900

    123

---
+++ Chap8/js/main.js
@@ -0,0 +1,22 @@
+import {gridData} from "./data.js"
+var container = document.getElementById('map');                 //지도를 그릴 element
+var options = {
+    center: new kakao.maps.LatLng(37.151198243701934, 128.22723681773422),
+    level: 12
+};                                                              //지도의 설정 정보
+
+var map = new kakao.maps.Map(container, options);               // container element에 맵 객체 생성
+
+var windmap = new Windmap(map, gridData, 31, 44, 115, 138, 0.5);
+
+window.onload = () => {
+    windmap.init();
+}
+
+windmap.addControler();
+
+
+document.getElementById('playWind').addEventListener('click', windmap.toggleWindLayer)
+
+
+
